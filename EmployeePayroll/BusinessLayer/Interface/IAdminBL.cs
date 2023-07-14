a4f469eacd64daeb4560f8181bad1d0eb390a408
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:09:22 +0530

    [Tanaya] : Created Admin Service Interface in Business Layer

---
+++ EmployeePayroll/BusinessLayer/Interface/IAdminBL.cs
@@ -0,0 +1,14 @@
+﻿using CommonLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IAdminBL
+    {
+        public AdminModel AddAdmin(AdminModel registerAdmin);
+        public int LoginAdminGetAdminId(AdminModel loginAdmin);
+        //public int LoginAdminGetAdminCount(AdminModel loginAdmin);
+    }
+}
