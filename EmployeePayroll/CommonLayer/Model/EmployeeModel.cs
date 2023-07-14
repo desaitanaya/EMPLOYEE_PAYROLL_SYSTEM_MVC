839d9717e90443c45c1b2bdeebe9d1aefc690fb2
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:08:17 +0530

    [Tanaya] : Created EmployeeModel Class for managing Employee data

---
+++ EmployeePayroll/CommonLayer/Model/EmployeeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public string ProfileImage { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public string Department { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public decimal Salary { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        
+        public DateTime CreatedAt { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public DateTime UpdatedAt { get; set; }
+        [Required(ErrorMessage = "{0} field is required")]
+        public string Notes { get; set; }
+
+
+    }
+}
