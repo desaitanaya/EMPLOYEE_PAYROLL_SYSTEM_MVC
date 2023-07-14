4a0644918eea802126f8f5a45047131c56d999b0
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:09:56 +0530

    [Tanaya] : Implemented Admin Service methods

---
+++ EmployeePayroll/BusinessLayer/Service/AdminBL.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class AdminBL : IAdminBL
+    {
+        private readonly IAdminRepo adminRepository;
+
+        public AdminBL(IAdminRepo adminRepository)
+        {
+            this.adminRepository = adminRepository;
+        }
+
+        public AdminModel AddAdmin(AdminModel adminModel)
+        {
+            try
+            {
+                return adminRepository.AddAdmin(adminModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public int LoginAdminGetAdminId(AdminModel adminModel)
+        {
+            try
+            {
+                return adminRepository.LoginAdminGetAdminId(adminModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        //public int LoginAdminGetAdminCount(AdminModel adminModel)
+        //{
+        //    try
+        //    {
+        //        return adminRepository.LoginAdminGetAdminCount(adminModel);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+
+        //}
+
+    }
+}
