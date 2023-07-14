bcb083e17db915fb4fd38ac6dd427b71f415ce51
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:07:44 +0530

    [Tanaya] : Created AdminModel Class for managing Admin data

---
+++ EmployeePayroll/CommonLayer/Model/AdminModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class AdminModel
+    {
+        public int AdminID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
