926a82f476732d2203c00c220596411a4172f28b
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:11:11 +0530

    [Tanaya] : Created Admin Repository Interface in Repository Layer

---
+++ EmployeePayroll/RepositoryLayer/Interface/IAdminRepo.cs
@@ -0,0 +1,14 @@
+﻿using CommonLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IAdminRepo
+    {
+        public AdminModel AddAdmin(AdminModel registerAdmin);
+        public int LoginAdminGetAdminId(AdminModel loginAdmin);
+        //public int LoginAdminGetAdminCount(AdminModel loginAdmin);
+    }
+}
