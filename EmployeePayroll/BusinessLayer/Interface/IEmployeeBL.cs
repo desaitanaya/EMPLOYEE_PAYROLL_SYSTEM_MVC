b6240cf9474d9e38492b02da76826ed278f47cf3
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:09:07 +0530

    [Tanaya] : Created Employee Service Interface in Business Layer

---
+++ EmployeePayroll/BusinessLayer/Interface/IEmployeeBL.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IEmployeeBL
+    {
+        public EmployeeModel AddEmployee(EmployeeModel registerEmployee);
+        public int LoginEmployee(EmployeeModel loginEmployee);
+        public IEnumerable<EmployeeModel> GetAllEmployees();
+        public EmployeeModel GetEmployeeById(int id);
+        public EmployeeModel EditEmployee(EmployeeModel employee);
+        public EmployeeModel DeleteEmployee(int id);
+    }
+}
