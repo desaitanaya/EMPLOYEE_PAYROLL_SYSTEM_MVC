b80408e1044cfff4bf52ffacbcad089ae2d823a7
Tanaya <desaitanaya17@gmail.com>
2023-07-14 10:11:27 +0530

    [Tanaya] : Created Employee Repository Interface in Repository Layer

---
+++ EmployeePayroll/RepositoryLayer/Interface/IEmployeeRepo.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IEmployeeRepo
+    {
+        public EmployeeModel AddEmployee(EmployeeModel registerEmployee);
+        public int LoginEmployee(EmployeeModel loginEmployee);
+        public IEnumerable<EmployeeModel> GetAllEmployees();
+        public EmployeeModel GetEmployeeById(int id);
+        public EmployeeModel EditEmployee(EmployeeModel employee);
+        public EmployeeModel DeleteEmployee(int id);
+    }
+}
